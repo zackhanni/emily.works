56e7b82b781203e5bfabcbd872e0362cab8b3d0e
Zack Hanni <zack.hannI@gmail.com>
2024-04-13 18:51:50 -0400

    Create page.tsx

---
+++ app/commercial/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ProjectTemplate from "../components/ProjectTemplate";
+
+export default function Commercial() {
+  return (
+    <ProjectTemplate
+      title="Commercial Projects"
+      description="This is the description section. Im putting text here as a placeholder until emily gives me more content."
+      firm="Firm: Eileen Tognini Design; Position: Designer and FF+E Specialist; "
+      client="Client: Client name here"
+    />
+  );
+}
