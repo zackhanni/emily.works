0ffae63829ad45b8265fa38bd9f258b4e8d5cf5e
Zack Hanni <zack.hannI@gmail.com>
2024-04-13 19:07:15 -0400

    started to add bootstrap structuring

--- app/components/ProjectTemplate.tsx
+++ app/components/ProjectTemplate.tsx
@@ -10,11 +10,13 @@ interface ProjectTemplateProps {
 export default function ProjectTemplate(props: ProjectTemplateProps) {
   return (
     <main>
-      <section>
-        <h2>{props.title}</h2>
-        <p>{props.description}</p>
-        <p>{props.firm}</p>
-        <p>{props.client}</p>
+      <section className="container py-8">
+        <div className="row text-center">
+          <h2 className="pb-8">{props.title}</h2>
+          <p>{props.description}</p>
+          <p>{props.firm}</p>
+          <p>{props.client}</p>
+        </div>
       </section>
     </main>
   );
