73e8e6a95c9d46825a7f58ab3e637decf082a8da
Zack Hanni <zack.hannI@gmail.com>
2024-04-13 19:06:12 -0400

    update nav links

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -2,7 +2,7 @@ export default function Nav() {
   return (
     <nav className="container navbar navbar-expand-md">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">
+        <a className="navbar-brand" href="/">
           Emily Hanni
         </a>
         {/* mobile button */}
@@ -17,7 +17,7 @@ export default function Nav() {
         <div className="d-none d-md-flex">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link" href="#">
+              <a className="nav-link" href="/about">
                 About
               </a>
             </li>
@@ -33,12 +33,12 @@ export default function Nav() {
               </a>
               <ul className="dropdown-menu">
                 <li>
-                  <a className="dropdown-item" href="#">
+                  <a className="dropdown-item" href="/commercial">
                     Commercial
                   </a>
                 </li>
                 <li>
-                  <a className="dropdown-item" href="#">
+                  <a className="dropdown-item" href="/residential">
                     Residential
                   </a>
                 </li>
@@ -46,7 +46,7 @@ export default function Nav() {
                   <hr className="dropdown-divider" />
                 </li>
                 <li>
-                  <a className="dropdown-item" href="#">
+                  <a className="dropdown-item" href="/">
                     Model units
                   </a>
                 </li>
